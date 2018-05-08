d37a6dda77a2d3a76c12893f2f93b2f85f8bec11
Andrew Le <andrew_khang_le@hotmail.com>
2018-05-08 17:35:16 +0000

    update dburl

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-//mongoose.connect("mongodb://localhost/yelp_camp_v10");
-mongoose.connect("mongodb://andrew:masterof12@ds053126.mlab.com:53126/yelpcamp");
+
+//Databases    
+mongoose.connect(process.env.DATABASEURL);
+//mongoose.connect("mongodb://andrew:masterof12@ds053126.mlab.com:53126/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
